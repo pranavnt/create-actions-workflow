3d628013f7d315a8a81579aaac30ab5f77606d81
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-05 23:46:35 -0800

    worked on langlist

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "langChoice",
+      message: "Which language is this project in?",
+      choices: [
+        "Javascript (including node)",
+        "Go",
+        "Python",
+        "Java",
+        "C++",
+        "C#",
+        "C",
+        "Kotlin",
+        "Swift",
+        "Ruby on Rails",
+      ],
+    },
+  ])
+  .then((answers) => {
+    console.info("Answer:", answers.langChoice);
+  });
