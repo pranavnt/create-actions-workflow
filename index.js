b64f851278b0367a11a3ab46d6671a9c60e791a9
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-06 00:51:43 -0800

    added fs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 inquirer
   .prompt([
@@ -16,7 +17,6 @@ inquirer
         "C",
         "Kotlin",
         "Swift",
-        "Ruby on Rails",
       ],
     },
   ])
