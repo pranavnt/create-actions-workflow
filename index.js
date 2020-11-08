ae9495c05f8616bcdfc53a1b5bac31b626d39b39
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-07 18:55:09 -0800

    build cmd ig

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+// dependencies
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 
-const createDirs = require("./helpers/createDirs");
-
+// inquirer prompt
 inquirer
   .prompt([
+    // language selection
     {
       type: "list",
       name: "langChoice",
@@ -22,12 +23,19 @@ inquirer
         "Swift",
       ],
     },
+    // dependency installation
     {
       type: "list",
       name: "nodeDependency",
       message: "What dependency manager are you using",
       choices: ["NPM", "Yarn"],
       when: (answers) => answers.langChoice === "Javascript (including node)",
+    },
+    // build command
+    {
+      type: "input",
+      name: "buildCommand",
+      message: "What is your build command",
     },
   ])
   .then((answers) => {
